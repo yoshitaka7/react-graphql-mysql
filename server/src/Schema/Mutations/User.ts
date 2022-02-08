19da3a043979befed05685eacc6060bf1713fbd8
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2022-02-08 17:35:02 +0900

    chpater 2 end

---
+++ server/src/Schema/Mutations/User.ts
@@ -0,0 +1,59 @@
+import { GraphQLString, GraphQLID } from 'graphql';
+import { UserType } from '../TypeDefs/User';
+import { MessageType } from '../TypeDefs/Messages';
+import { Users } from '../../Entities/Users';
+
+export const CREATE_USER = {
+  type: UserType,
+  args: {
+    name: {type: GraphQLString},
+    username: {type: GraphQLString},
+    password: {type: GraphQLString},
+  },
+  resolve(parent: any, args: any) {
+    const { name, username, password } = args;
+    Users.insert({  name, username, password });
+    return args;
+  },
+}
+
+export const UPDATE_PASSWORD = {
+  type: MessageType,
+  args: {
+    username: {type: GraphQLString},
+    oldPassword: {type: GraphQLString},
+    newPassword: {type: GraphQLString},
+  },
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const user = await Users.findOne({ username: username });
+
+    if (!user) {
+      throw new Error("username doesnt exit")
+    }
+    const userPassword = user?.password;
+
+    if (oldPassword === userPassword) {
+      await Users.update(
+        { username: username },
+        { password: newPassword }
+      );
+      return { successful: true, message: "password update" };
+    } else {
+      throw new Error("password do not match")
+    }
+  }
+}
+
+export const DELETE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    const id = args.id
+    await Users.delete(id)
+
+    return { successful: true, message: "DELETE SUCCESS"}
+  },
+}
